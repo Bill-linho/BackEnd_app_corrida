394c0769e1a7ee9ceeefa7fa93a0872e2f80a45e
VITOR SANTOS PEREIRA <vitor_pereira153@estudante.sesisenai.org.br>
2026-09-01 16:53:48 -0300

    sequelizeEFindAll

--- back_end/src/controllers/atividade.controller.js
+++ back_end/src/controllers/atividade.controller.js
@@ -1,9 +1,12 @@
 import { Atividades } from '../models/atividade.models.js';
+import { User } from '../models/user.models.js';
 
 async function carregarAtividade(req, res) {
     try {
-        const atividades = await Atividades.findAll();
-        
+        const atividades = await Atividades.findAll({
+            include: [{ model: User, attributes: ['id', 'nome', 'email', 'imagem'] }]
+        });
+
         return res.status(200).json(atividades);
     } catch (error) {
         return res.status(500).json({ error: error.message });
